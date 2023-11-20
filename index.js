b04c93a72fa58a21858cbb6e77b98279e657b657
mohammed <mohammed.salad@student.moringaschool.com>
2023-11-20 15:00:36 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper' ;
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+function distanceFromHqInBlocks (F,T) {
+    console.log ((F-T))
+    return (F-T)
+
+}
+distanceFromHqInBlocks(50,42)
+
+function distanceFromHqInFeet () {
+    distanceFromHqInBlocks((50,42)*264)
+    return (distanceFromHqInBlocks)
+}
+distanceFromHqInFeet()
